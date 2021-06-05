ed1e24cae79768be57fa47f469da7641b1a4c9cf
Justin Grote <JustinGrote@users.noreply.github.com>
2021-06-05 11:06:20 -0700

    Even More

--- PowerConfig/Public/Add-PowerConfigHashTable.ps1
+++ PowerConfig/Public/Add-PowerConfigHashTable.ps1
@@ -7,7 +7,7 @@ class HashTableConfigurationProvider : ConfigurationProvider {
     hidden [HashTableConfigurationSource]$source
 
     HashTableConfigurationProvider ($source) {
-        $flatHashTable = ConvertTo-FlatHashTable $source.hashtable
+        $flatHashTable = ConvertTo-FlatDictionary $source.hashtable
         $flatHashTable.GetEnumerator().Foreach{
             $this.Set($PSItem.Name, $PSItem.Value)
         }
@@ -23,52 +23,6 @@ class HashTableConfigurationSource : IConfigurationSource {
 
     [IConfigurationProvider] Build([IConfigurationBuilder]$builder) {
         return [HashTableConfigurationProvider]::new($this)
-    }
-}
-
-function ConvertTo-FlatHashTable ([iDictionary]$Dictionary,[String]$delimiter) {
-<#
-.SYNOPSIS
-Converts a nested hashtable into a set of key value pairs
-#>
-    $nameValuePairs = Get-FlatNameValuePairs -Delimiter $delimiter -InputObject $Dictionary
-    $hashTable = @{}
-    $nameValuePairs.foreach{
-        $hashTable[$PSItem.Name] = $PSItem.Value
-    }
-    return $hashTable
-}
-
-function Get-FlatNameValuePairs {
-    [CmdletBinding()]
-    param(
-        $InputObject,
-        #Used for recursion to remember the path
-        $pathStack = [Stack]::new(),
-        $delimiter = ':'
-    )
-    process
-    {
-        if ($inputObject -is [iDictionary]) {
-            $inputObject.GetEnumerator().Foreach{
-                $pathStack.Push($PSItem.Name)
-                Get-FlatNameValuePairs $PSItem $PathStack
-            }
-        } elseif ($inputObject.Value -is [iDictionary]) {
-            $inputObject.Value.GetEnumerator().Foreach{
-                $pathStack.Push($PSItem.Name)
-                Get-FlatNameValuePairs $PSItem $PathStack
-            }
-            [void]$pathStack.Pop()
-        } else {
-            $pathStackArray = $pathStack.ToArray()
-            [Array]::Reverse($pathStackArray)
-            $keyName = $pathStackArray -join $delimiter
-            if ($pathStack.count -gt 0) {
-                [void]$pathStack.Pop()
-            }
-            return [DictionaryEntry]::new($keyName, $InputObject.Value)
-        }
     }
 }
 
