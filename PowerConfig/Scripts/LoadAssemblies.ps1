4e105d0017fc10e455b77b89c3ac7437f64129be
Justin Grote <JustinGrote@users.noreply.github.com>
2021-06-08 07:08:59 -0700

    🐛 Remove Redundant Assembly Loading (#6)

--- PowerConfig/Scripts/LoadAssemblies.ps1
+++ PowerConfig/Scripts/LoadAssemblies.ps1
@@ -1,6 +1,15 @@
 #This is needed because assemblies must be loaded before classes that reference them in Windows Powershell 5.1
 #It should be referenced from "ScriptsToProcess" in the manifest file
 
+#PSES on Windows 5.1 is currently unsupported
+try {
+    if ([Microsoft.Extensions.Configuration.ConfigurationBuilder].Assembly.Location -match 'PowershellEditorServices') {
+        throw [NotSupportedException]'Sorry, PowerConfig is currently not supported if Powershell Editor Services is loaded on Windows Powershell due to a conflict. See: https://github.com/PowerShell/PowerShellEditorServices/issues/1499'
+    }
+} catch {
+    if ($PSItem.FullyQualifiedErrorId -ne 'TypeNotFound') {throw}
+}
+
 <#
 .SYNOPSIS
 Used to add automatic binding redirection to related modules for Powerconfig to redirect CompilerServices to the net5.0 assembly version
